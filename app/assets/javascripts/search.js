da25a0c6088e5ec0a1bd6c0b1fad2dbab0696b7b
myanko01 <yuuka.nenneco01@gmail.com>
2019-02-18 20:36:52 +0900

    インクリメンタルサーチ修正

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,20 +1,32 @@
-$(function() {
+$(function(){
+  var user_list = $('#user-search-result');
+  var message_list = $('#chat-group-users')
 
-   function appendUser(user) {
-    var html = `
-                  <div class="chat-group-user clearfix">
-                    <p class="chat-group-user__name">${user.name}</p>
-                      <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
-                  </div>
-               `
-   return html;
+  function appendUser(user) {
+    var html =
+              `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">
+                ${user.name}
+                </p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加
+                </a>
+              </div>`
+    user_list.append(html);
+  };
+
+  function appendMembers (userId, userName) {
+
+    var html =
+              `<div class='chat-group-user clearfix js-chat-member' id='${userId}'>
+                <input name='group[user_ids][]' type='hidden' value="${userId}">
+                <p class='chat-group-user__name'>${userName}</p>
+                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+              </div>`
+    message_list.append(html);
   };
 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
-    console.log(input);
-  });
-});
 
     $.ajax({
       type: 'GET',
@@ -27,14 +39,27 @@ $(function() {
       $(".user-search-result").empty();
       if (users.length !== 0) {
         users.forEach(function(user){
-          var html = appendUser(user);
-          $(".user-search-result").append(html);
+          appendUser(user);
         });
       }
+
     })
 
     .fail(function(){
       alert('通信に失敗しました');
     });
-
   });
+
+    $(document).on("click", '.user-search-add', function(){
+      var userId = $(this).attr("data-user-id");
+      var userName = $(this).attr("data-user-name")
+        appendMembers(userId, userName);
+     $('.chat-group-users').append();
+       $(this).parent().remove();
+    });
+
+    $(document).on("click", '.user-search-remove', function(){
+      $input = $(this);
+      $input.parent().remove();
+      });
+});
