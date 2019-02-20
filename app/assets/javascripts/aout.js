6625dcb71ba5aea9bb61b5e5a73d0f113fe4cdbc
myanko01 <yuuka.nenneco01@gmail.com>
2019-02-20 19:03:02 +0900

    自動更新機能

---
+++ app/assets/javascripts/aout.js
@@ -0,0 +1,47 @@
+$(function(){
+  function buildHTML(message){
+    var imagehtml = message.image == null ? "" : `<img src="${message.image}" class="lower-message__image">`
+    var html = `<ul class="chat-contents" id="${user.id}" >
+                    <li id="chat-content">
+                    <div class="chat-content__header">
+                      <div class="chat-content__name">
+                      ${message.user_name}
+                      </div>
+                      <div class="chat-content__time">
+                      ${message.created_at}
+                      </div>
+                      <div class="chat-content__message">
+                      ${message.content}
+                      ${imagehtml}
+                      </div>
+                    </div>`
+    return html;
+  }
+
+$(function(){
+  setInterval(update, 5000);
+    function update(){
+        var last_id = $('#new_message').data('id');
+      $.ajax({
+        url: location.href,
+        type: 'GET',
+        data: {
+          id:last_id
+        },
+        dataType: 'json'
+      })
+      .done(function(date){
+        date.forEach(function(date){
+          var html = buildHTML(date);
+          $('.chat-content').append(html);
+        });
+      })
+      .fail(function(data){
+        alert('自動更新に失敗しました');
+      });
+      .always(() => {
+      $("send-btn").removeAttr("disabled");
+      });
+    }
+});
+});
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").delete(:current_user)
     respond_to do |format|
       format.html
       format.json
